820dc8cfb64a1a195ef77f1a26ca351caf71eaa8
traaitt | TRRXITTE <traaitt@icloud.com>
2025-12-18 16:24:51 +0100

    clear res.rc

--- src/dogecoin-build-static.sh
+++ src/dogecoin-build-static.sh
@@ -3,7 +3,10 @@ arch -x86_64 zsh -c '
 cd /Users/traaitt/Documents/GitHub/nyancoin
 
 PREFIX="$HOME/x86"
-export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_LIBDIR"
+export PATH="$PREFIX/bin:/usr/local/bin:$PATH"
+export CPP="/usr/bin/cpp"
 export BOOST_ROOT="$PREFIX" BOOST_INCLUDEDIR="$PREFIX/include" BOOST_LIBDIR="$PREFIX/lib"
 export OPENSSL_PREFIX="$PREFIX" BDB_PREFIX="$PREFIX" LIBEVENT_PREFIX="$PREFIX" ZMQ_PREFIX="$PREFIX"
 export CPPFLAGS="-I$PREFIX/include -std=c++14"
