d81d3947098c8240056ecd65f7db8b51287dc1a0
traaitt | TRRXITTE <traaitt@icloud.com>
2025-12-22 00:42:18 +0100

    Revert "Reapply "ci""
    This reverts commit d7db72fb9ef3dbfe64b1ff5e25ea7d07965545f3.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -143,7 +143,7 @@ public:
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
-         * The characters are rarely used upper ASCII, not valid as UTF-8, and produceok
+         * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
         pchMessageStart[0] = 0xa4;
