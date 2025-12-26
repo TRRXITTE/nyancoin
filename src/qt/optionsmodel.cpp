50efb171cf58811dda4d3d08610e9b7f426ef6c7
traaitt | TRRXITTE <traaitt@icloud.com>
2025-12-26 04:16:22 +0100

    update explorer

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -75,7 +75,7 @@ void OptionsModel::Init(bool resetSettings)
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "https://dogechain.info/tx/%s|https://chain.so/tx/DOGE/%s");
+        settings.setValue("strThirdPartyTxUrls", "https://mempool.nintondo.trrxitte.com/tx/%s|https://index.nintondo.trrxitte.com/tx/%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
